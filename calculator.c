80059f0f060c30adf2b2f97f5285fecc275b0f90
aditidesai25 <desaiaditi8383@gmail.com>
2019-12-05 12:48:18 -0800

    modified commited

--- calculator.c
+++ calculator.c
@@ -1,25 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,ch;
-	printf("enter two numbers: ");
-	scanf("%d%d",&a,&b);
-	printf("1.addition 2.subtraction 3.multiplication 4.exit")
-	printf("enter your choice");
-	scanf("%d",ch);
-	switch(ch!=5)
-	{
-		case 1:
-			int res=a+b;
-			printf("addition is: %d ",res);
-		case 2:
-			int res1=a-b;
-			printf("subtraction is:%d ",res1);
-		case 3: 
-			int re2=a*b;
-			printf("multiplication is:%d",res2);
-		case 4:
-			exit;
-	}
+	int a=30;
+        int b=20;
+	printf("addition is:%d",a+b);
+	printf("subtraction is:%d",a-b);
+	printf("multiplication is:%d"a*b);	
 	return 0;
 }
